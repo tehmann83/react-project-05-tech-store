0b3b6a0203caf08de0e03e4b759eee10a9d26bd9
Thomas Ehmann <ehmannth@gmail.com>
2019-09-29 15:56:37 +0200

    Added pages and folder structure

--- src/App.js
+++ src/App.js
@@ -2,6 +2,13 @@ import React from "react";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 
+import Home from "./pages/HomePage";
+import About from "./pages/AboutPage";
+import Contact from "./pages/ContactPage";
+import Products from "./pages/ProductsPage";
+import SingleProduct from "./pages/SingleProductPage";
+import Default from "./pages/DefaultPage";
+
 function App() {
   return <h1>tech store</h1>;
 }
