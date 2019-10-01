e038da0381ea0299ff5b86676acd1ab49bb34dac
Thomas Ehmann <ehmannth@gmail.com>
2019-10-01 20:11:08 +0200

    Added Contact component

---
+++ src/components/ContactPage/Contact.js
@@ -0,0 +1,48 @@
+import React from "react";
+import Title from "../Title";
+
+export default function Contact() {
+  return (
+    <section className="py-5">
+      <div className="row">
+        <div className="col-10 mx-auto col-md-6 my-3">
+        <Title title="contact us"></Title>
+          <form className="mt-5">
+            <div className="form-group">
+              <input
+                type="text"
+                name="firstName"
+                className="form-control"
+                placeholder="Your name"
+              />
+            </div>
+            <div className="form-group">
+              <input
+                type="email"
+                name="email"
+                className="form-control"
+                placeholder="Your email"
+              />
+            </div>
+            <div className="form-group">
+              <input
+                type="text"
+                name="subject"
+                className="form-control"
+                placeholder="Subject"
+              />
+            </div>
+            <div className="form-group">
+              <textarea
+                name="message"
+                className="form-control"
+                rows="10"
+                placeholder="Your message"
+              ></textarea>
+            </div>
+          </form>
+        </div>
+      </div>
+    </section>
+  );
+}
