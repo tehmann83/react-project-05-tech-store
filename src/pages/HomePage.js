91a25fe89300d7aabc58ec4fa7331a3f1dd0b125
Thomas Ehmann <ehmannth@gmail.com>
2019-10-01 23:45:34 +0200

    Added Services to HomePage

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -2,6 +2,8 @@ import React from "react";
 import { ProductConsumer } from "../context/Context";
 import Hero from "../components/Hero";
 import { Link } from "react-router-dom";
+import Services from "../components/HomePage/Services";
+import Featured from "../components/HomePage/Featured";
 
 export default function HomePage() {
   return (
@@ -14,8 +16,18 @@ export default function HomePage() {
           return (
             <>
               <Hero title="awesome gadgets" max="true">
-                <Link to="/products" className="main-link" style={{margin: "2rem"}}>our products</Link>
+                <Link
+                  to="/products"
+                  className="main-link"
+                  style={{ margin: "2rem" }}
+                >
+                  our products
+                </Link>
               </Hero>
+              <Services>
+          
+              </Services>
+              <Featured></Featured>
             </>
           );
         }}
