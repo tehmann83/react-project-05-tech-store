e07976ce66d69ddd57edf8a5a054fa3bd145a993
Thomas Ehmann <ehmannth@gmail.com>
2019-10-04 17:07:43 +0200

    Added SideCart component

--- src/components/SideCart.js
+++ src/components/SideCart.js
@@ -1,15 +1,39 @@
 import React from "react";
 import { ProductConsumer } from "../context/Context";
 import styled from "styled-components";
+import { Link } from "react-router-dom";
 
 export default function SideCart() {
   return (
     <ProductConsumer>
       {value => {
-        const { cartOpen, closeCart, cart } = value;
+        const { cartOpen, closeCart, cart, cartTotal } = value;
         return (
           <CartWrapper show={cartOpen} onClick={closeCart}>
-            <p>cart items here</p>
+            <ul>
+              {cart.map(item => {
+                console.log("item: ", item);
+                return (
+                  <li key={item.id} className="cart-item mb-4">
+                    <img width="35" src={`../${item.image}`}/*{item.image}*/ alt={item.title} />
+                    <div className="mt-3">
+                      <h6 className="text-uppercase">{item.title}</h6>
+                      <h6 className="text-title text-capitalize">
+                        amount: {item.count}
+                      </h6>
+                    </div>
+                  </li>
+                );
+              })}
+            </ul>
+            <h4 className="text-capitalize text-main">
+              cart total: ${cartTotal}
+            </h4>
+            <div className="text-center mb-5">
+              <Link to="/cart" className="main-link">
+                cart page
+              </Link>
+            </div>
           </CartWrapper>
         );
       }}
@@ -28,6 +52,16 @@ const CartWrapper = styled.div`
   border-left: 4px solid var(--primaryColor);
   transition: var(--mainTransition);
   transform: ${props => (props.show ? "translateX(0)" : "translateX(100%)")};
+  overflow: scroll;
+  padding: 2rem;
+
+  ul {
+    padding: 0 !important;
+  }
+
+  .cart-item {
+    list-style-type: none;
+  }
 
   @media (min-width: 576px) {
     width: 20rem;
